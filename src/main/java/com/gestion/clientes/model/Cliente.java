db579ce7fea83b8fc80b12b3a663edb58a0b7b8d
dnunezf <david.a.nunez27@outlook.com>
2025-05-06 09:09:19 -0600

    AAA

---
+++ src/main/java/com/gestion/clientes/model/Cliente.java
@@ -0,0 +1,83 @@
+package com.gestion.clientes.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+@Entity
+@Table(name = "clientes")
+public class Cliente
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "email")
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(long id, String nombre, String apellido, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public Cliente(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
